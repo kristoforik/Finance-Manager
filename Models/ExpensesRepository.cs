2acc820080b006dc2c0b981eedbacdc51304cbf6
Andrii <kudenko4614@gmail.com>
2023-03-29 10:16:06 -0400

    Create Models(Classes) for Notes, Reminders, Earnings, Expenses and their repositories

---
+++ Models/ExpensesRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finance_Manager.Models
+{
+    //Categories: Housing, Transportation, Food, Health/fitness, Personal care, Education, Debt payments, Insuarance, Gifts/donations
+    public enum Category
+    {
+        Housing, Transportation, Food, HealthFitness, PersonalCare, Education, DebtPayments, Insuarance, GiftsDonations
+    }
+    public class ExpensesRepository
+    {
+
+    }
+}
